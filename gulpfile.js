d10fb05255f9bd0458e027aa273f7e1f64ae0ce5
Anthony Johnson <aj@ohess.org>
2015-02-08 16:29:19 -0800

    Initial commit of package layout and testing

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    gulp_util = require('gulp-util'),
+    debowerify = require('debowerify');
+
+gulp.task('browserify', function () {
+    gulp.src(['lib/readthedocs.js'])
+        .pipe(browserify({
+            transform: ['debowerify']
+        }))
+        .on('error', function (event) {
+            gulp_util.log(event.message);
+        })
+        .pipe(gulp.dest('dist/'));
+});
+
+/* Tasks */
+gulp.task('build', ['browserify']);
+
+gulp.task('dev', ['build', 'watch']);
+
+gulp.task('watch', function () {
+    gulp.watch('lib/*.js', ['build']);
+});
+
+gulp.task('default', ['build']);
